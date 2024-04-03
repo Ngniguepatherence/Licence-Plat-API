f57dea254f8bd09e8e0d95dbed678b8f1efa0fb2
Ngniguepa Therence <ngniguepafaha@gmail.com>
2024-04-03 02:38:49 +0100

    fir

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 import uvicorn
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+HOST = '0.0.0.0'
 
 if __name__ == "__main__":
-    uvicorn.run("server.api:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("server.api:app", host=HOST, port=8000, reload=True)
